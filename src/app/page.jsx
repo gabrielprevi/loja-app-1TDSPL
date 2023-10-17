4231e8d3aa3048f63585114ba2bec78c303ca43d
gabrielprevi <gabriel.previ@gmail.com>
2023-10-17 09:29:10 -0300

    iniciando projeto da 4ª entrega

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <> 
+    <h1>Tênis</h1>
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta unde maiores ullam eveniet. Et, sit corrupti. In quisquam saepe placeat, sit exercitationem sint iure animi eos dolorem, eius id fugiat?
+    </div>
+    </>
+  )    
+}
